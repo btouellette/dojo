788ae4fa4deed4e6a83126a69364e1b32a3b8209
govtpiggy <govtpiggy@f0ee62be-4a59-cf38-381d-971ca1e890f6>
2007-10-08 20:07:44 +0000

    Text panel now working correctly and styled

--- trunk/L5R Online/Main.java
+++ trunk/L5R Online/Main.java
@@ -28,32 +28,12 @@ public class Main
 		menuBar.setOpaque(true);
 		//menuBar.setBackground(Color.CYAN);
 		menuBar.setPreferredSize(new Dimension(width, 25));
-
-		//Create the main play area
-        PlayArea playArea = new PlayArea();
-        playArea.setOpaque(true);
-        playArea.setBackground(Color.WHITE);
-        playArea.setPreferredSize(new Dimension(width, height));
-
-        //Create the chat/info area
-        JPanel infoArea = new JPanel();
-        infoArea.setOpaque(true);
-        infoArea.setBackground(Color.LIGHT_GRAY);
-        infoArea.setPreferredSize(new Dimension(width, 175));
-        //Add a pretty border to it
-		infoArea.setBorder(BorderFactory.createLoweredBevelBorder());
-		infoArea.setLayout(new BorderLayout());
 		
-		//Create the chat area
-		JEditorPane chatBox = new JEditorPane();
-		JTextField chatSend = new JTextField();
-		chatBox.setEditable(false);
-		chatBox.setContentType("text/rtf");
-		JScrollPane scrollPane = new JScrollPane(chatBox);
-		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-		chatSend.addActionListener(new TextActionListener(chatBox));
-		infoArea.add(scrollPane, BorderLayout.CENTER);
-		infoArea.add(chatSend, BorderLayout.SOUTH);
+		//Create the play area
+		PlayArea playArea = createPlayArea(width, height);
+
+        //Create the info area (card box, chat box, game info box)
+        JPanel infoArea = createInfoArea(width, height);
 
 		//Create a split panel so that the relative size of the areas can be user controlled
 		JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, true, playArea, infoArea);
@@ -69,7 +49,42 @@ public class Main
         frame.pack();
         frame.setVisible(true);
     }
+    
+	private static PlayArea createPlayArea(int width, int height)
+	{
+		//Create the main play area
+        PlayArea playArea = new PlayArea();
+        playArea.setOpaque(true);
+        playArea.setBackground(Color.WHITE);
+        playArea.setPreferredSize(new Dimension(width, height));
+        
+        return playArea;
+	}
 
+	private static JPanel createInfoArea(int width, int height)
+	{
+		//Create the chat/info area
+        JPanel infoArea = new JPanel();
+        infoArea.setOpaque(true);
+        infoArea.setBackground(Color.LIGHT_GRAY);
+        infoArea.setPreferredSize(new Dimension(width, 175));
+        //Add a pretty border to it
+		infoArea.setBorder(BorderFactory.createLoweredBevelBorder());
+		infoArea.setLayout(new BorderLayout());
+		
+		//Create the chat area
+		JTextPane chatBox = new JTextPane();
+		JTextField chatSend = new JTextField();
+		chatBox.setEditable(false);
+		JScrollPane scrollPane = new JScrollPane(chatBox);
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		chatSend.addActionListener(new TextActionListener(chatBox));
+		infoArea.add(scrollPane, BorderLayout.CENTER);
+		infoArea.add(chatSend, BorderLayout.SOUTH);
+		
+		return infoArea;
+	}
+	
     public static void main(String[] args)
     {
         //Schedule a job for the event-dispatching thread:
