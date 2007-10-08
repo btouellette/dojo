2fdd5de9b41df5f12aed82373b44b1c63566c507
govtpiggy <govtpiggy@f0ee62be-4a59-cf38-381d-971ca1e890f6>
2007-10-08 20:07:44 +0000

    Text panel now working correctly and styled

--- L5R Online/TextActionListener.java
+++ L5R Online/TextActionListener.java
@@ -8,20 +8,38 @@
  
 package l5r;
 
+import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.text.*;
 
 public class TextActionListener implements ActionListener{
 
-	private JEditorPane output;
+	private JTextPane output;
 	private final static String newline = "\n";
 	
-    public TextActionListener(JEditorPane chatBox)
+    public TextActionListener(JTextPane chatBox)
     {
     	//Recieves the textfield to which the ActionListener outputs
     	output = chatBox;
+    	addStyles();
     	output.setText("Welcome to L5R Online!");
+    }
+    
+    public void addStyles()
+    {
+    	StyledDocument doc = output.getStyledDocument();
+    	
+    	Style style = output.addStyle("Default", null);
+    
+	    style = output.addStyle("Action", null);
+	    StyleConstants.setForeground(style, new Color(99, 204, 33));
+	    
+	    style = output.addStyle("YourName", null);
+	    StyleConstants.setForeground(style, Color.BLUE);
+	    
+	    style = output.addStyle("OppName", null);
+	    StyleConstants.setForeground(style, Color.RED);
     }
     
     public void actionPerformed(ActionEvent e)
@@ -32,6 +50,12 @@ public class TextActionListener implements ActionListener{
     public void send(String text)
     {
     	//Just updates the chat area locally for now, needs to be updated to send a network command
-    	output.setText(output.getText() + newline + text);
+    	try	{
+    		StyledDocument doc = output.getStyledDocument();
+    		//Default will be replaced with the appropriate style
+    		doc.insertString(doc.getLength(), newline + text, doc.getStyle("Default"));
+    	} catch(BadLocationException e)	{
+    		System.err.println("The following error ocured: " + e);
+    	}
     }
 }
