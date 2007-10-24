b166c425f13c85c53eb9cb4b9e6e691926e706bc
govtpiggy <govtpiggy@f0ee62be-4a59-cf38-381d-971ca1e890f6>
2007-10-24 19:10:47 +0000

    Read in images for possible display in CardInfoBox

--- L5R Online/CardInfoBox.java
+++ L5R Online/CardInfoBox.java
@@ -2,12 +2,14 @@
 // Written by Brian Ouellette
 // Part of Dojo
 
+//TODO: Add a JPopUpMenu for detaching the panel
+
 package l5r;
 
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.text.*;
-import java.io.IOException;
+import java.io.*;
 
 class CardInfoBox extends JEditorPane
 {
@@ -27,7 +29,35 @@ class CardInfoBox extends JEditorPane
 
     public void setCard(Card card)
     {
-		String cardHTML1, cardHTML2, cardHTML3, cardHTML4;
+		String cardHTML1, cardHTML2, cardHTML3, cardHTML4, cardImageLoc = "";
+		boolean imageExists = false, moreImages = true;
+		int count = 0;
+
+
+		/* Commented out until it's determined whether images should be present in the info box
+		//Find the first valid image location
+		while(!imageExists && moreImages)
+		{
+			cardImageLoc = card.getImageLocation(count);
+			if(cardImageLoc.equals(""))
+			{
+				moreImages = false;
+			}
+			else
+			{
+				imageExists = (new File(cardImageLoc)).exists();
+			}
+			count++;
+		}
+
+		//Hack to get a non-relative file location as JEditorPane doesn't support relative pathnames in IMG tags
+		//If used then <img src=\"" + cardImageLoc + "\"> is added to the HTML
+		if(imageExists)
+		{
+			File temp = new File(cardImageLoc);
+			cardImageLoc = "file:" + temp.getAbsolutePath();
+		}
+		*/
 
 		cardHTML1 = "";
 		cardHTML2 = "";
@@ -37,49 +67,50 @@ class CardInfoBox extends JEditorPane
 		if(card.getType().equals("actions") || card.getType().equals("kihos"))
 		{
 			//The card text in the window is created as an html table
-			cardHTML1 = "<html><center><table><tr><td colspan=\"5\"><center><b>" + card.getName() + "</b></center></td></tr>";
+			//Complete sytnax isn't necessary as setText() reformats it
+			cardHTML1 = "<center><table><tr><td colspan=\"5\"><center><b>" + card.getName() + "</b></center></td></tr>";
 			cardHTML2 = "<tr><td colspan = \"5\"><center>" + card.getCost() + " G</center></td><td></td></tr>";
 			cardHTML3 = "<tr><td colspan = \"5\">" + card.getText() + "</td></tr>";
-			cardHTML4 = "<tr><td colspan = \"5\"><center>" + card.getFocus() + " Focus</center></td></tr></table></center></html>";
+			cardHTML4 = "<tr><td colspan = \"5\"><center>" + card.getFocus() + " Focus</center></td></tr></table></center>";
 		}
 		//TODO: Use String.replaceAll and regexp to insert &middot instead of . in the traits section
 		else if(card.getType().equals("ancestors") || card.getType().equals("followers"))
 		{
-			cardHTML1 = "<html><center><table><tr><td><center>" + card.getForce() + "F</center></td><td colspan=\"3\"><center><b>" + card.getName() + "</b></center></td><td><center>" + card.getChi() + "C</center></td></tr>";
+			cardHTML1 = "<center><table><tr><td><center>" + card.getForce() + "F</center></td><td colspan=\"3\"><center><b>" + card.getName() + "</b></center></td><td><center>" + card.getChi() + "C</center></td></tr>";
 			cardHTML2 = "<tr><td></td><td colspan = \"3\"><center>" + card.getHonorReq() + " / " + card.getCost() + " / " + card.getPersonalHonor() + "</center></td><td></td></tr>";
 			cardHTML3 = "<tr><td></td><td colspan = \"3\"><center>HR / GC / PH<center></td><td></td></tr>";
-			cardHTML4 = "<tr><td colspan = \"5\">" + card.getText() + "</td></tr><tr><td colspan = \"5\"><center>" + card.getFocus() + " Focus</center></td></tr></table></center></html>";
+			cardHTML4 = "<tr><td colspan = \"5\">" + card.getText() + "</td></tr><tr><td colspan = \"5\"><center>" + card.getFocus() + " Focus</center></td></tr></table></center>";
 		}
 		else if(card.getType().equals("events") || card.getType().equals("regions") || card.getType().equals("winds"))
 		{
-			cardHTML1 = "<html><center><table><tr><td colspan=\"5\"><center><b>" + card.getName() + "</b></center></td></tr>";
-			cardHTML2 = "<tr><td colspan = \"5\">" + card.getText() + "</td></tr></table></html>";
+			cardHTML1 = "<center><table><tr><td colspan=\"5\"><center><b>" + card.getName() + "</b></center></td></tr>";
+			cardHTML2 = "<tr><td colspan = \"5\">" + card.getText() + "</td></tr></table>";
 		}
 		else if(card.getType().equals("holdings"))
 		{
-			cardHTML1 = "<html><center><table><tr><td colspan=\"5\"><center><b>" + card.getName() + "</b></center></td></tr>";
+			cardHTML1 = "<center><table><tr><td colspan=\"5\"><center><b>" + card.getName() + "</b></center></td></tr>";
 			cardHTML2 = "<tr><td colspan = \"5\"><center>" + card.getCost() + " G</center></td><td></td></tr>";
-			cardHTML3 = "<tr><td colspan = \"5\">" + card.getText() + "</td></tr></table></center></html>";
+			cardHTML3 = "<tr><td colspan = \"5\">" + card.getText() + "</td></tr></table></center>";
 		}
 		else if(card.getType().equals("items") || card.getType().equals("spells"))
 		{
-			cardHTML1 = "<html><center><table><tr><td><center>" + card.getForce() + "F</center></td><td colspan=\"3\"><center><b>" + card.getName() + "</b></center></td><td><center>" + card.getChi() + "C</center></td></tr>";
+			cardHTML1 = "<center><table><tr><td><center>" + card.getForce() + "F</center></td><td colspan=\"3\"><center><b>" + card.getName() + "</b></center></td><td><center>" + card.getChi() + "C</center></td></tr>";
 			cardHTML2 = "<tr><td></td><td colspan = \"3\"><center>" + card.getCost() + " G</center></td><td></td></tr>";
 			cardHTML3 = "<tr><td></td><td colspan = \"5\">" + card.getText() + "</td><td></td></tr>";
-			cardHTML4 = "<tr><td colspan = \"5\"><center>" + card.getFocus() + " Focus</center></td></tr></table></center></html>";
+			cardHTML4 = "<tr><td colspan = \"5\"><center>" + card.getFocus() + " Focus</center></td></tr></table></center>";
 		}
 		else if(card.getType().equals("personalities"))
 		{
-			cardHTML1 = "<html><center><table><tr><td><center>" + card.getForce() + "F</center></td><td colspan=\"3\"><center><b>" + card.getName() + "</b></center></td><td><center>" + card.getChi() + "C</center></td></tr>";
+			cardHTML1 = "<center><table><tr><td><center>" + card.getForce() + "F</center></td><td colspan=\"3\"><center><b>" + card.getName() + "</b></center></td><td><center>" + card.getChi() + "C</center></td></tr>";
 			cardHTML2 = "<tr><td></td><td colspan = \"3\"><center>" + card.getHonorReq() + " / " + card.getCost() + " / " + card.getPersonalHonor() + "</center></td><td></td></tr>";
 			cardHTML3 = "<tr><td></td><td colspan = \"3\"><center>HR / GC / PH</center></td><td></td></tr>";
-			cardHTML4 = "<tr><td colspan = \"5\">" + card.getText() + "</td></tr></table></center></html>";
+			cardHTML4 = "<tr><td colspan = \"5\">" + card.getText() + "</td></tr></table></center>";
 		}
 		else if(card.getType().equals("rings"))
 		{
-			cardHTML1 = "<html><center><table><tr><td colspan=\"5\"><center><b>" + card.getName() + "</b></center></td></tr>";
+			cardHTML1 = "<center><table><tr><td colspan=\"5\"><center><b>" + card.getName() + "</b></center></td></tr>";
 			cardHTML2 = "<tr><td colspan = \"5\">" + card.getText() + "</td></tr>";
-			cardHTML3 = "<tr><td colspan = \"5\"><center>" + card.getFocus() + " Focus</center></td></tr></table></center></html>";
+			cardHTML3 = "<tr><td colspan = \"5\"><center>" + card.getFocus() + " Focus</center></td></tr></table></center>";
 		}
 		else if(card.getType().equals("senseis"))
 		{
@@ -92,6 +123,7 @@ class CardInfoBox extends JEditorPane
 
 		//Set the card window to show the html table for the card
 		setText(cardHTML1 + cardHTML2 + cardHTML3 + cardHTML4);
+		System.out.println(getText());
 		//Scroll the window back up to the top
 		setCaretPosition(0);
 	}
