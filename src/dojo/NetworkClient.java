c51a8fa486d559da472569be717dee6a40d59391
btouellette <btouellette@f0ee62be-4a59-cf38-381d-971ca1e890f6>
2010-08-26 17:07:26 +0000

    Adding JSON library. Starting on Egg interoperability

---
+++ src/dojo/NetworkClient.java
@@ -0,0 +1,17 @@
+package dojo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class NetworkClient
+{
+	// Protocol version
+	int versionNumber = 8;
+	
+	public NetworkClient()
+	{
+	}
+}
