21da7ed0e916447bd3c03180d50d648fa2fe358b
btouellette <btouellette@f0ee62be-4a59-cf38-381d-971ca1e890f6>
2010-08-27 05:11:39 +0000

    Little bit more networking.

--- trunk/src/dojo/TextActionListener.java
+++ trunk/src/dojo/TextActionListener.java
@@ -6,6 +6,9 @@ package dojo;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.text.BadLocationException;
@@ -16,6 +19,8 @@ import javax.swing.text.StyledDocument;
 class TextActionListener implements ActionListener
 {
 	private JTextPane chatBox;
+	// Locks the updating of the chat box so multiple threads can send updates
+	private static Lock lock = new ReentrantLock();
 
 	public TextActionListener()
 	{
@@ -52,18 +57,26 @@ class TextActionListener implements ActionListener
 		//Clear the text box
 		textBox.setText("");
 	}
-
+	
 	public static void send(String text, String style)
 	{
 		//TODO: Just updates the chat area locally for now, needs to be updated to send a network command
+		update(text, style);
+	}
+
+	public static void update(String text, String style)
+	{
+		// Can't guarantee the Swing component is thread safe and multiple clients may try to update the chatbox at the same time
+		lock.lock();
 		try
 		{
 			StyledDocument doc = Main.chatBox.getStyledDocument();
+			// Add the string to the end of the chatbox, appropriately styled
 			doc.insertString(doc.getLength(), "\n" + text, doc.getStyle(style));
-		}
-		catch(BadLocationException e)
-		{
+		} catch(BadLocationException e) {
 			System.err.println("The following error ocured: " + e);
+		} finally {
+			lock.unlock();
 		}
 	}
 }
