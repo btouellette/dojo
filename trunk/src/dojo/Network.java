96399d2640c575d410301128e81c3a4ab126bc52
btouellette <btouellette@f0ee62be-4a59-cf38-381d-971ca1e890f6>
2010-08-27 03:26:09 +0000

    More networking progress

--- trunk/src/dojo/Network.java
+++ trunk/src/dojo/Network.java
@@ -1,10 +1,15 @@
 package dojo;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
+import java.util.Vector;
+
 import org.json.*;
 
 public class Network
@@ -17,43 +22,75 @@ public class Network
 	public Network()
 	{
 		ServerSocket serverSocket;
+		// Using a vector since it is thread safe
+		List<NetworkHandler> connections = new Vector<NetworkHandler>();
 		try {
-			serverSocket = new ServerSocket(gamePort);
-			Socket clientSocket = null;
-			try {
-				clientSocket = serverSocket.accept();
-				System.out.println("Accept succeeded.");
-			} catch (IOException e) {
-				System.err.println("Accept failed.");
-			}
-			
-			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-			
-			String outputLine;
-			JSONArray jarray;
-			while (true)
+			while(true)
 			{
-				outputLine = in.readLine();
-				if(outputLine != null)
-				{
-					jarray = new JSONArray(outputLine);
-					String command = jarray.getString(0);
-					if(command.equals("protocol"))
-					{
-						handleProtocol(jarray.getJSONObject(1));
-					}
+				serverSocket = new ServerSocket(gamePort);
+				Socket clientSocket = null;
+				try {
+					clientSocket = serverSocket.accept();
+					NetworkHandler nh = new NetworkHandler(clientSocket, connections);
+					nh.start();
+					connections.add(nh);
+					System.out.println("Accept succeeded.");
+				} catch (IOException e) {
+					System.err.println("Accept failed.");
 				}
 			}
 		} catch (IOException e) {
 			System.err.println("Could not listen on port: " + gamePort + ".");
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
+		}
+	}
+
+private class NetworkHandler extends Thread
+{
+	private BufferedReader in;
+	private BufferedWriter out;
+	private List<NetworkHandler> connections;
+	
+	public NetworkHandler(Socket s, List<NetworkHandler> connections)
+	{
+		this.connections = connections;
+		try {
+			in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+		} catch (IOException e) {
 			e.printStackTrace();
+			System.err.println("** Failed to get in/out stream to client");
+		}
+	}
+	
+	public void run()
+	{
+		while (true)
+		{
+			try {
+				String outputLine = in.readLine();
+				if(outputLine != null)
+				{
+					try {
+						JSONArray jarray = new JSONArray(outputLine);
+						String command = jarray.getString(0);
+						if(command.equals("protocol"))
+						{
+							handleProtocol(jarray.getJSONObject(1));
+						}
+					} catch (JSONException e) {
+						e.printStackTrace();
+						System.err.println("** Failed to parse JSON command from client");
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.err.println("** Couldn't get new line from client");
+			}
 		}
 	}
 	
 	// Encode values into a JSON compatible string to send over the network
-	public String encode(String type, String key, int value) throws JSONException
+	private String encode(String type, String key, int value) throws JSONException
 	{
 		JSONWriter object = new JSONStringer().object()
 		                                      .key(key)
@@ -68,7 +105,7 @@ public class Network
 	}
 	
 	// Encode values into a JSON compatible string to send over the network
-	public String encode(String type, String key, String value) throws JSONException
+	private String encode(String type, String key, String value) throws JSONException
 	{
 		JSONWriter object = new JSONStringer().object()
 		                                      .key(key)
@@ -82,12 +119,34 @@ public class Network
 		return message;
 	}
 	
-	public void sendProtocol() throws JSONException
+	public void send(String message)
 	{
-		String message = encode("protocol", "version", protocolVersion);
+		try {
+			out.write(message);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("** Failed to send: " + message);
+		}
+	}
+
+	private void broadcast(String message)
+	{
+		for(NetworkHandler nh : connections)
+		{
+			if(!nh.equals(this))
+			{
+				nh.send(message);
+			}
+		}
 	}
 	
-	public void handleProtocol(JSONObject jobj) throws JSONException
+	private void sendProtocol() throws JSONException
+	{
+		String message = encode("protocol", "version", protocolVersion);
+		send(message);
+	}
+	
+	private void handleProtocol(JSONObject jobj) throws JSONException
 	{
 		if(jobj.getInt("version") == protocolVersion )
 		{
@@ -98,6 +157,8 @@ public class Network
 			// We've encountered a different protocol version
 			// Report back failure to the client
 			String message = encode("rejected", "msg", "Your client protocol version is wrong (got " + jobj.getInt("version") + ", needs " + protocolVersion + ")");
+			send(message);
 		}
 	}
+}
 }
